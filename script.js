e393374d0efe34f3d27eb387f8cfcbd3cbbba911
DortchCasey <caseyd199@gmail.com>
2020-01-11 16:25:46 -0700

    Repairs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*Simple js to dynamicaly get the copyright year 
 and the time the document was last modified. */
 let lastModif = new Date(document.lastModified);
-let curYear= new Date;
-document.getElementById('copyYear').innerHTML=curYear;
+let curYear= new Date();
+document.getElementById('copyYear').innerHTML=curYear.getFullYear;
 document.getElementById('lastModYear').innerHTML=lastModif;
