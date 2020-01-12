eaf34fa2f799c19030d1802c84c2822f7aed699c
DortchCasey <caseyd199@gmail.com>
2020-01-11 20:28:39 -0700

    Error checking

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 /*Simple js to dynamicaly get the copyright year 
 and the time the document was last modified. */
-let lastModif = new Date(document.lastModified);
-let curYear= new Date();
+var lastModif = new Date(document.lastModified);
+var curYear= new Date();
 window.onload = function(){
     loadYear();
     function loadYear(){
     document.getElementById('copyRightYear').innerHTML=curYear.getFullYear();
     document.getElementById('lastModYear').innerHTML=lastModif;
     }
-}
+};
 /*end*/
