33ef5e787ec4034ca4ba79fd1142d8a87f88bc66
DortchCasey <caseyd199@gmail.com>
2020-02-04 22:12:00 -0700

    JS fixes and CSS organizing

--- weather/weatherScript.js
+++ weather/weatherScript.js
@@ -1,12 +1,16 @@
 /*Simple js to dynamicaly get the copyright year 
 and the time the document was last modified. */
 var lastModif = new Date(document.lastModified);
-var curYear= new Date();
+var curDate= new Date();
+var days=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var months=['January', 'February', 'March', 'April', 'May','June', 'July', 'August', 'September',
+'October', 'November', 'December']
 window.onload = function(){
     loadYear();
     function loadYear(){
     document.getElementById('copyRightYear').innerHTML=curYear.getFullYear();
-    document.getElementById('lastModYear').innerHTML=lastModif;
+    document.getElementById('lastModYear').innerHTML=days[lastModif.getDay()]+", "+lastModif.getDate()+
+        " "+months[lastModif.getMonth()]+" "+lastModif.getFullYear(); //Wednesday, 24 July 2020
     }
     /*for the responsive hamburger menu*/
 
